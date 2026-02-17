171f5e6486663503d4ed86b25ebd4a829bd20ef3
Yamini_Bathini <vtu26151@veltech.edu.in>
2026-02-17 15:28:02 +0530

    Tasks

---
+++ WEEK-5/session-21/SpringBootJDBC17/src/main/java/com/demo/jdbc17/SpringBootJdbc17Application.java
@@ -0,0 +1,13 @@
+package com.demo.jdbc17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbc17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbc17Application.class, args);
+	}
+
+}
