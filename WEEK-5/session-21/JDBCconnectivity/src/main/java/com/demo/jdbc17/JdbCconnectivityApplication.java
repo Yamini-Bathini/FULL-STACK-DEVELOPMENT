0e8f9dab1d9d55257c5e388095f7af13c9f4449c
Yamini_Bathini <vtu26151@veltech.edu.in>
2026-02-19 11:17:40 +0530

    Tasks

--- WEEK-5/session-21/JDBCconnectivity/src/main/java/com/demo/jdbc17/JdbCconnectivityApplication.java
+++ WEEK-5/session-21/JDBCconnectivity/src/main/java/com/demo/jdbc17/JdbCconnectivityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJdbc17Application {
+public class JdbCconnectivityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJdbc17Application.class, args);
+		SpringApplication.run(JdbCconnectivityApplication.class, args);
 	}
 
 }
--- WEEK-5/session-21/JDBCconnectivity/src/test/java/com/demo/jdbc17/JdbCconnectivityApplicationTests.java
+++ WEEK-5/session-21/JDBCconnectivity/src/test/java/com/demo/jdbc17/JdbCconnectivityApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootJdbc17ApplicationTests {
+class JdbCconnectivityApplicationTests {
 
 	@Test
 	void contextLoads() {
